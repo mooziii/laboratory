4f0898f20ee7da5ddceaf94302ca23f73300c4c9
mooziii <63669478+mooziii@users.noreply.github.com>
2022-08-01 12:13:08 +0200

    Fix path resolving

--- src/main/kotlin/me/obsilabor/laboratory/platform/impl/CustomJarPlatform.kt
+++ src/main/kotlin/me/obsilabor/laboratory/platform/impl/CustomJarPlatform.kt
@@ -22,7 +22,7 @@ object CustomJarPlatform : IPlatform {
 
     override suspend fun downloadJarFile(path: Path, mcVersion: String, build: String): Boolean {
         val customJarPath = Path.of(terminal.prompt("Please enter the path to your custom jarfile"))
-        if (Files.exists(path)) {
+        if (Files.exists(customJarPath)) {
             Files.copy(customJarPath, path)
         } else {
             terminal.println(TextColors.brightRed("Invalid path."))
